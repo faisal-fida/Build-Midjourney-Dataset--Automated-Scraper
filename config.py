2beadd225f2358e2f953113bcefa3cf0bca11d92
Faisal Fida <fa20-bcs-004@cuiwah.edu.pk>
2024-10-09 09:33:31 +0500

    initial commit

---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+RETRY_AFTER_DEFAULT = 60
+SLEEP_MIN = 0.8
+SLEEP_MAX = 1.5
+SAVE_INTERVAL = 300 * 300
+DATA_DIR = "data"
+DATA_FILE = os.path.join(DATA_DIR, "messages.json")
+MAX_RETRIES = 5
+RETRY_DELAY = 5
+
+
+def load_config():
+    with open("data/config.json") as f:
+        config = json.load(f)
+        return config
+
+
+def save_config(params):
+    config = load_config()
+    config["params"] = params
+    with open("data/config.json", "w") as f:
+        json.dump(config, f)
